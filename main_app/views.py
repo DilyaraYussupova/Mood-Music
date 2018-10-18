8145ccb5141944c7a5d9cbbd6885748457f1d5e1
Your Name <you@example.com>
2018-10-18 12:24:54 -0700

    Bootstrap added

--- main_app/views.py
+++ main_app/views.py
@@ -105,21 +105,3 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
-
-def add_photo(request, playlist_id):
-	# photo-file was the "name" attribute on the <input type="file">
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            photo = Photo(url=url, playlist_id=playlist_id)
-            photo.save()
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('detail', playlist_id=playlist_id)
